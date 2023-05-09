e3af055f84983ae7e7707dad0a7a3243127c41fb
Atick Faisal <atickfaisal@gmail.com>
2023-05-09 13:49:49 +0300

    Finish serial comm

--- firmware/Smart-Insole-Firmware/src/main.cpp
+++ firmware/Smart-Insole-Firmware/src/main.cpp
@@ -1,17 +1,42 @@
 #include <Arduino.h>
 
+#include "BluetoothSerial.h"
+
+// ... Bluetooth Check
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled!
+#endif
+
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Bluetooth not available or not enabled.
+#endif
+
+#define USE_PIN
+
+const char *BT_PIN = "1234";
+const char *DEVICE_NAME = "SMART_INSOLE_RIGHT";
 const uint8_t ADDRESS_PINS[] = {14, 15, 27, 26};
 const uint8_t SENSOR_PIN = 25;
 const uint8_t ENABLE_PIN = 4;
-
 uint16_t pressureValues[16];
+char serializedPressureValues[100];
+
+BluetoothSerial SerialBT;
 
 void readValuesFromMUX(const uint8_t *addressPins, const uint8_t sensorPin,
                        uint16_t *sensorValues);
+void serializePressureValues(char *buffer, uint16_t *pressureValues,
+                             size_t nValues);
 void printPressureValues();
 
 void setup() {
     Serial.begin(115200);
+    SerialBT.begin(DEVICE_NAME);
+    Serial.printf("Deive %s is now Available\n", DEVICE_NAME);
+
+#ifdef USE_PIN
+    SerialBT.setPin(BT_PIN);
+#endif
 
     // ... Enable MUX
     pinMode(ENABLE_PIN, OUTPUT);
@@ -25,7 +50,8 @@ void setup() {
 
 void loop() {
     readValuesFromMUX(ADDRESS_PINS, SENSOR_PIN, pressureValues);
-    printPressureValues();
+    serializePressureValues(serializedPressureValues, pressureValues, 16);
+    SerialBT.println(serializedPressureValues);
     delay(1000);
 }
 
@@ -36,6 +62,18 @@ void readValuesFromMUX(const uint8_t *addressPins, const uint8_t sensorPin,
             digitalWrite(addressPins[i], bitRead(ch, i));
         }
         sensorValues[ch] = analogRead(sensorPin);
+    }
+}
+
+void serializePressureValues(char *buffer, uint16_t *pressureValues,
+                             size_t nValues) {
+    buffer[0] = '\0';
+    sprintf(buffer, "R: ");
+    for (int i = 0; i < nValues; ++i) {
+        sprintf(buffer + strlen(buffer), "%d", pressureValues[i]);
+        if (i != nValues - 1) {
+            sprintf(buffer + strlen(buffer), ",");
+        }
     }
 }
 
--- utils/serialutils.py
+++ utils/serialutils.py
@@ -12,14 +12,14 @@ def get_serial_data(serial_port: serial.Serial) -> List[float] | None:
         
         print(values)
 
-        # return list(map(float, values)) + list(map(float, values))
+        return list(map(float, values)) + list(map(float, values))
 
-        return [
-            4000, 4000, 4000, 4095, 4095, 4095, 4095, 3000, 3000,
-            0, 2000, 0, 0, 0, 0, 0,
-            3000, 3000, 3000, 3500, 3500, 3500, 3500, 3000, 3000,
-            0, 2000, 0, 0, 4095, 4095, 4095,
-        ]
+        # return [
+        #     4000, 4000, 4000, 4095, 4095, 4095, 4095, 3000, 3000,
+        #     0, 2000, 0, 0, 0, 0, 0,
+        #     3000, 3000, 3000, 3500, 3500, 3500, 3500, 3000, 3000,
+        #     0, 2000, 0, 0, 4095, 4095, 4095,
+        # ]
 
     except ValueError:
         return None
